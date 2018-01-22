004e16fcda70b934fe52952202b944a0ed7ef0fe
viktor <vik.melnikov@gmail.com>
2018-01-22 13:55:51 +0300

    fix response error

--- src/Request.php
+++ src/Request.php
@@ -383,7 +383,7 @@ class Request
                 $msg = $exception->getRequest();
 
                 if ( $exception->hasResponse() ) {
-                    $msg = $exception->getResponse();
+                    $msg = $exception->getResponse()->getBody()->getContents();
                 }
             }
         }
